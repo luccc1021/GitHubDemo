f9f78148d3a5641143e8bf9fcc5301a42170ddde
470301559 <470301559@qq.com>
2017-07-05 17:18:50 +0800

    新更新了一个类

--- app/src/main/java/com/bway/githubdemo/Wz.java
+++ app/src/main/java/com/bway/githubdemo/Wz.java
@@ -1,10 +1,29 @@
 package com.bway.githubdemo;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * 功能描述  :
  * 创建时间 : 2017/7/5 17:12
  * 编写人  :  王成哲
  */
 
-public class Wz {
+public class Wz extends View{
+    public Wz(Context context) {
+        super(context);
+    }
+
+    public Wz(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public Wz(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Wz(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 }
