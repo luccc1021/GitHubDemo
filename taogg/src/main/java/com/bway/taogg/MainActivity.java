5b2ffa5726109a2b3af639a4a27477cb0ae6897c
lenovo <962229493@qq.com>
2017-07-05 16:13:12 +0800

    涛哥哥新建的分支1

---
+++ taogg/src/main/java/com/bway/taogg/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bway.taogg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //你好呀 顶顶顶
+    }
+}
